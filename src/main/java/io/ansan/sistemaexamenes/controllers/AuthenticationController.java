460d5fb4eb822282dd128f207b409a74b5e6c2cd
Miguel Martinez <miguel.emezero@gmail.com>
2022-09-09 17:49:46 -0500

    class authority

---
+++ src/main/java/io/ansan/sistemaexamenes/controllers/AuthenticationController.java
@@ -0,0 +1,78 @@
+package io.ansan.sistemaexamenes.controllers;
+
+import io.ansan.sistemaexamenes.config.JwtUtils;
+import io.ansan.sistemaexamenes.entity.JwtRequest;
+import io.ansan.sistemaexamenes.entity.JwtResponse;
+import io.ansan.sistemaexamenes.services.UserService;
+import io.ansan.sistemaexamenes.services.impl.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@CrossOrigin("*")
+public class AuthenticationController {
+  @Autowired
+  private AuthenticationManager authenticationManager;
+
+  @Autowired
+  private UserDetailsServiceImpl userDetailsService;
+
+  @Autowired
+  private JwtUtils jwtUtils;
+
+  @Autowired
+  private UserService userService;
+
+  @Autowired
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+  @RequestMapping(value = "/generate-token", method = RequestMethod.POST)
+  public ResponseEntity<?> generateToken(@RequestBody JwtRequest  jwtRequest) {
+    try {
+      authenticate(jwtRequest.getUsername(), jwtRequest.getPassword());
+    } catch (UsernameNotFoundException e) {
+      return ResponseEntity.badRequest().body("Username not found");
+    }
+
+    var userDetails = this.userDetailsService.loadUserByUsername(jwtRequest.getUsername());
+    var jwt = jwtUtils.generateToken(userDetails);
+
+    return ResponseEntity.ok(new JwtResponse(jwt));
+  }
+
+  public void authenticate (String username, String password) {
+    var localUser = userService.getUser(username);
+    if (localUser == null) {
+      throw new UsernameNotFoundException("User not found");
+    }
+
+    //var match = bCryptPasswordEncoder.matches(password, localUser.getPassword());
+    var match = localUser.getPassword().equals(password);
+    if (!match) {
+      throw new BadCredentialsException("Incorrect password");
+    }
+
+    System.out.println("Correct");
+  }
+
+
+  @GetMapping("/current-user")
+  public ResponseEntity<?> getCurrentUser(HttpServletRequest request) {
+    var token = request.getHeader("Authorization");
+    if(token == null) {
+      return ResponseEntity.badRequest().body("Token not found");
+    }
+    token = token.substring(7);
+    var username = jwtUtils.extractUsername(token);
+    var userDetails = this.userDetailsService.loadUserByUsername(username);
+    return ResponseEntity.ok(userDetails);
+  }
+
+}
